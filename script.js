530b8845907e6477ea56e20e89be7a02dd4a882c
waliegal <egalwali@gmail.com>
2022-04-24 14:14:34 -0600

    every button now leads to thank you page

--- script.js
+++ script.js
@@ -1,2 +1,73 @@
+let main = document.querySelector('.main')
+let page1 = document.querySelector('.rating1')
+let page2 = document.querySelector('.rating2')
+let page3 = document.querySelector('.rating3')
+let page4 = document.querySelector('.rating4')
+let page5 = document.querySelector('.rating5')
 
 
+document.querySelector('.one').addEventListener('click', color1)
+document.querySelector('.two').addEventListener('click', color2)
+document.querySelector('.three').addEventListener('click', color3)
+document.querySelector('.four').addEventListener('click', color4)
+document.querySelector('.five').addEventListener('click', color5)
+
+function color1() {
+    // document.querySelector(".one").style.backgroundColor = "hsl(25, 97%, 53%)";
+    // document.querySelector(".one").style.color = "white";
+    document.querySelector('.main').style.display = "none"
+    document.querySelector('.rating2').style.display = "none"
+    document.querySelector('.rating3').style.display = "none"
+    document.querySelector('.rating4').style.display = "none"
+    document.querySelector('.rating5').style.display = "none"
+
+}
+
+function color2() {
+    // document.querySelector(".one").style.backgroundColor = "hsl(25, 97%, 53%)";
+    // document.querySelector(".one").style.color = "white";
+    document.querySelector('.main').style.display = "none"
+    document.querySelector('.rating1').style.display = "none"
+    document.querySelector('.rating3').style.display = "none"
+    document.querySelector('.rating4').style.display = "none"
+    document.querySelector('.rating5').style.display = "none"
+
+}
+
+function color3() {
+    // document.querySelector(".one").style.backgroundColor = "hsl(25, 97%, 53%)";
+    // document.querySelector(".one").style.color = "white";
+    document.querySelector('.main').style.display = "none"
+    document.querySelector('.rating2').style.display = "none"
+    document.querySelector('.rating1').style.display = "none"
+    document.querySelector('.rating4').style.display = "none"
+    document.querySelector('.rating5').style.display = "none"
+
+}
+
+function color4() {
+    // document.querySelector(".one").style.backgroundColor = "hsl(25, 97%, 53%)";
+    // document.querySelector(".one").style.color = "white";
+    document.querySelector('.main').style.display = "none"
+    document.querySelector('.rating2').style.display = "none"
+    document.querySelector('.rating3').style.display = "none"
+    document.querySelector('.rating1').style.display = "none"
+    document.querySelector('.rating5').style.display = "none"
+
+}
+
+function color5() {
+    // document.querySelector(".one").style.backgroundColor = "hsl(25, 97%, 53%)";
+    // document.querySelector(".one").style.color = "white";
+    document.querySelector('.main').style.display = "none"
+    document.querySelector('.rating2').style.display = "none"
+    document.querySelector('.rating3').style.display = "none"
+    document.querySelector('.rating4').style.display = "none"
+    document.querySelector('.rating1').style.display = "none"
+
+}
+
+
+// function one(){
+    
+// }
