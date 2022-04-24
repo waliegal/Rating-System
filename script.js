3beda43933aaf85e91229df0c3db29dc8d6c616d
waliegal <egalwali@gmail.com>
2022-04-24 15:09:42 -0600

    added eventlistener

--- script.js
+++ script.js
@@ -12,62 +12,68 @@ document.querySelector('.three').addEventListener('click', color3)
 document.querySelector('.four').addEventListener('click', color4)
 document.querySelector('.five').addEventListener('click', color5)
 
-function color1() {
-    // document.querySelector(".one").style.backgroundColor = "hsl(25, 97%, 53%)";
-    // document.querySelector(".one").style.color = "white";
-    main.style.display = "none"
-    page2.style.display = "none"
-    page3.style.display = "none"
-    page4.style.display = "none"
-    page5.style.display = "none"
 
+function color1() {
+    document.querySelector('.submit').addEventListener('click', submit1)
+    document.querySelector(".one").style.backgroundColor = "hsl(25, 97%, 53%)";
+    document.querySelector(".one").style.color = "white";
+    function submit1 (){
+        main.style.display = "none"
+        page2.style.display = "none"
+        page3.style.display = "none"
+        page4.style.display = "none"
+        page5.style.display = "none"
+    }
 }
 
 function color2() {
-    // document.querySelector(".one").style.backgroundColor = "hsl(25, 97%, 53%)";
-    // document.querySelector(".one").style.color = "white";
-    main.style.display = "none"
-    page1.style.display = "none"
-    page3.style.display = "none"
-    page4.style.display = "none"
-    page5.style.display = "none"
-
+    document.querySelector('.submit').addEventListener('click', submit1)
+    document.querySelector(".two").style.backgroundColor = "hsl(25, 97%, 53%)";
+    document.querySelector(".two").style.color = "white";
+    function submit1 (){
+        main.style.display = "none"
+        page1.style.display = "none"
+        page3.style.display = "none"
+        page4.style.display = "none"
+        page5.style.display = "none"
+    }
 }
 
 function color3() {
-    // document.querySelector(".one").style.backgroundColor = "hsl(25, 97%, 53%)";
-    // document.querySelector(".one").style.color = "white";
-    main.style.display = "none"
-    page2.style.display = "none"
-    page1.style.display = "none"
-    page4.style.display = "none"
-    page5.style.display = "none"
-
+    document.querySelector('.submit').addEventListener('click', submit1)
+    document.querySelector(".three").style.backgroundColor = "hsl(25, 97%, 53%)";
+    document.querySelector(".three").style.color = "white";
+    function submit1 (){
+        main.style.display = "none"
+        page1.style.display = "none"
+        page2.style.display = "none"
+        page4.style.display = "none"
+        page5.style.display = "none"
+    }
 }
 
 function color4() {
-    // document.querySelector(".one").style.backgroundColor = "hsl(25, 97%, 53%)";
-    // document.querySelector(".one").style.color = "white";
-    main.style.display = "none"
-    page2.style.display = "none"
-    page3.style.display = "none"
-    page1.style.display = "none"
-    page5.style.display = "none"
-
+    document.querySelector('.submit').addEventListener('click', submit1)
+    document.querySelector(".four").style.backgroundColor = "hsl(25, 97%, 53%)";
+    document.querySelector(".four").style.color = "white";
+    function submit1 (){
+        main.style.display = "none"
+        page1.style.display = "none"
+        page2.style.display = "none"
+        page3.style.display = "none"
+        page5.style.display = "none"
+    }
 }
 
 function color5() {
-    // document.querySelector(".one").style.backgroundColor = "hsl(25, 97%, 53%)";
-    // document.querySelector(".one").style.color = "white";
-    main.style.display = "none"
-    page2.style.display = "none"
-    page3.style.display = "none"
-    page4.style.display = "none"
-    page1.style.display = "none"
-
+    document.querySelector('.submit').addEventListener('click', submit1)
+    document.querySelector(".five").style.backgroundColor = "hsl(25, 97%, 53%)";
+    document.querySelector(".five").style.color = "white";
+    function submit1 (){
+        main.style.display = "none"
+        page1.style.display = "none"
+        page2.style.display = "none"
+        page3.style.display = "none"
+        page4.style.display = "none"
+    }
 }
-
-
-// function one(){
-    
-// }
